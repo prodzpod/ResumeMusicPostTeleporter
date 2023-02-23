862f0128b38efc6b8d0b4e6e30706ff24b852c78
prodzpod <product0339@gmail.com>
2023-02-24 06:00:48 +0900

    feat: delay

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Resume Music Post Teleporter
 Resumes music after the teleporter is fully charged, instead of silence. Through config, you can also set it to not stop teleporter music and only resume specific set of music.
+
+## Changelog
+- 1.0.1: Added configurable delay
+- 1.0.0: Initial Release
--- ResumeMusicPostTeleporter/ResumeMusicPostTeleporterPlugin.cs
+++ ResumeMusicPostTeleporter/ResumeMusicPostTeleporterPlugin.cs
@@ -5,6 +5,7 @@ using RoR2;
 using RoR2.WwiseUtils;
 using System.Collections.Generic;
 using System.Linq;
+using UnityEngine;
 
 namespace ResumeMusicPostTeleporter
 {
@@ -14,14 +15,16 @@ namespace ResumeMusicPostTeleporter
         public const string PluginGUID = PluginAuthor + "." + PluginName;
         public const string PluginAuthor = "prodzpod";
         public const string PluginName = "ResumeMusicPostTeleporter";
-        public const string PluginVersion = "1.0.0";
+        public const string PluginVersion = "1.0.1";
         public static ManualLogSource Log;
         internal static PluginInfo pluginInfo;
         public static ConfigFile Config;
         public static ConfigEntry<bool> UseTeleporterMusic;
         public static ConfigEntry<bool> SubstituteOtherMusic;
         public static List<string> ResumeMusicList;
+        public static ConfigEntry<float> ResumeDelay;
         public static bool musicPlaying;
+        public static float resumeDelay = 0;
 
         public void Awake()
         {
@@ -31,9 +34,10 @@ namespace ResumeMusicPostTeleporter
             UseTeleporterMusic = Config.Bind("General", "Use Teleporter Music Instead", false, "If true, changes functionality to not stop teleporter music post charge.");
             SubstituteOtherMusic = Config.Bind("General", "Substitute Other Music for Blacklist", true, "If true, if the main/boss music is in the blacklist, resume the other one if possible.");
             ResumeMusicList = Config.Bind("General", "Resumable Music Blacklist", "", "Music to not resume, separated by comma. See logs to find currently playing song's internal name.").Value.Split(',').ToList().ConvertAll(x => x.Trim());
+            ResumeDelay = Config.Bind("General", "Music Resume Delay", 5f, "Seconds to wait before resuming music.");
             On.RoR2.MusicTrackDef.Play += (orig, self) => { orig(self); Log.LogDebug("Current Song: " + (self.cachedName ?? "None")); };
             On.RoR2.MusicTrackDef.Stop += (orig, self) => { orig(self); Log.LogDebug("Previous Song: " + (self.cachedName ?? "None")); };
-            Stage.onStageStartGlobal += _ => musicPlaying = false;
+            Stage.onStageStartGlobal += _ => { musicPlaying = false; resumeDelay = 0; };
             On.RoR2.MusicController.UpdateTeleporterParameters += (orig, self, teleporter, cameraTransform, targetBody) =>
             {
                 orig(self, teleporter, cameraTransform, targetBody);
@@ -43,6 +47,8 @@ namespace ResumeMusicPostTeleporter
             MusicController.pickTrackHook += (MusicController self, ref MusicTrackDef track) =>
             {
                 if (!(TeleporterInteraction.instance?.isCharged ?? false)) return;
+                resumeDelay += Time.deltaTime;
+                if (resumeDelay < ResumeDelay.Value) return;
                 List<MusicTrackDef> list = new() { SceneCatalog.mostRecentSceneDef.mainTrack, SceneCatalog.mostRecentSceneDef.bossTrack };
                 if (UseTeleporterMusic.Value) list.Reverse();
                 if (!SubstituteOtherMusic.Value) list.RemoveAt(1);
